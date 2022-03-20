9ce55901d2c022ce9acdfa32c0b0bdcf99de0a76
Nico <johnnicomolon@yahoo.com>
2022-03-20 20:41:51 +0800

    delete method

--- src/main/java/com/projecttracker/repository/ProjectRepository.java
+++ src/main/java/com/projecttracker/repository/ProjectRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.projecttracker.model.Project;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-	
 }
