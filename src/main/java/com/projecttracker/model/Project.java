f0c687ba56cc443e62c75f272650bdb174a08c09
Nico <johnnicomolon@yahoo.com>
2022-03-20 20:41:23 +0800

    Add persistent anotation

--- src/main/java/com/projecttracker/model/Project.java
+++ src/main/java/com/projecttracker/model/Project.java
@@ -1,13 +1,17 @@
 package com.projecttracker.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name = "project")
@@ -21,11 +25,31 @@ public class Project {
 	@Column(name="name")
 	private String name;
 	
-	@ManyToOne
-	@JoinColumn(name = "todo_id", nullable = false)
-	private Todo toDo;
+	@OneToMany(mappedBy = "project")
+	@JsonManagedReference
+	private List<Todo> todoList = new ArrayList<Todo>();
 	
-	public Todo getToDo() {
-		return this.toDo;
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<Todo> getTodoList() {
+		return todoList;
+	}
+
+	public void setTodoList(List<Todo> todoList) {
+		this.todoList = todoList;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
 	}
 }
--- src/main/java/com/projecttracker/model/Todo.java
+++ src/main/java/com/projecttracker/model/Todo.java
@@ -5,17 +5,51 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 @Table(name = "todo")
 public class Todo {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Long id;
-	
-	@Column(name="name")
+
+	@Column(name = "name")
 	private String name;
+
+	public Long getId() {
+		return id;
+	}
+	
+	@ManyToOne
+	@JoinColumn(name="project_id")
+	@JsonBackReference 
+	private Project project;
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
