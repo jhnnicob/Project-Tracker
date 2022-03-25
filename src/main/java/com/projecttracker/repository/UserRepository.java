27b6f45451cb73c90edf4dffac74a5e259e2b543
Nico <johnnicomolon@yahoo.com>
2022-03-25 14:46:24 +0800

    Add method

--- src/main/java/com/projecttracker/repository/UserRepository.java
+++ src/main/java/com/projecttracker/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.projecttracker.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.projecttracker.exception.ServiceException;
 import com.projecttracker.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findUserByUsername(String username) throws ServiceException;
 	
 }
