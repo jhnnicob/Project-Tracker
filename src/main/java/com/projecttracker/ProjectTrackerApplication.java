1f82e93cf4e58da29bf96ae11672b9bf46d6b1e4
Nico <johnnicomolon@yahoo.com>
2022-03-25 14:43:41 +0800

    Remove bean

--- src/main/java/com/projecttracker/ProjectTrackerApplication.java
+++ src/main/java/com/projecttracker/ProjectTrackerApplication.java
@@ -2,6 +2,8 @@ package com.projecttracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ProjectTrackerApplication {
@@ -9,5 +11,4 @@ public class ProjectTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectTrackerApplication.class, args);
 	}
-
 }
