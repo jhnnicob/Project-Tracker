db5bdf411bf4f4a842ba31e877e7434a0ad0e0ce
Nico <johnnicomolon@yahoo.com>
2022-04-05 10:56:13 +0800

    Change repository to service

--- src/main/java/com/projecttracker/service/UserService.java
+++ src/main/java/com/projecttracker/service/UserService.java
@@ -4,5 +4,5 @@ import com.projecttracker.exception.ServiceException;
 import com.projecttracker.model.User;
 
 public interface UserService {
-	User findUserByUsername(String username) throws ServiceException;
+	User findByUsername(String username) throws ServiceException;
 }
