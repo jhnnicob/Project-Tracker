54400edb6f1039466c6231157c99464fe45225dc
Nico <johnnicomolon@yahoo.com>
2022-04-05 14:27:08 +0800

    Add Delete

--- src/main/java/com/projecttracker/controller/ProjectController.java
+++ src/main/java/com/projecttracker/controller/ProjectController.java
@@ -3,7 +3,9 @@ package com.projecttracker.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,6 +45,20 @@ public class ProjectController {
 		try {
 			Object data = projectRepository.findAll();
 			restResult.setData(data);
+		} catch (Exception e) {
+			e.printStackTrace();
+			restResult.setMessage(e.getMessage());
+			restResult.setStackTrace(ExceptionUtil.toString(e));
+		}
+		return restResult;
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public @ResponseBody RestResult deleteProject(@PathVariable Long id) {
+		RestResult restResult = RestResult.negativeInstance();
+		try {
+			projectRepository.deleteById(id);
+			restResult.setSuccess(true);
 		} catch (Exception e) {
 			e.printStackTrace();
 			restResult.setMessage(e.getMessage());
